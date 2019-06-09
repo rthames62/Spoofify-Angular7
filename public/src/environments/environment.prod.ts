71b2ca36a7c6b4c0075bc236f4389f616776241c
Ryan Thames <ryan@Ryans-MacBook-Pro.local>
2019-06-08 22:13:49 -0400

    change prod api url

--- public/src/environments/environment.prod.ts
+++ public/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  serverBaseUrl: 'https://spoofifyclone.herokuapp.com:3000/api',
+  serverBaseUrl: 'https://spoofify-api.herokuapp.com/api',
   spotifyApiBaseUrl: 'https://api.spotify.com/v1'
 };
